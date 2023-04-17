5ebcaee9a5713fdd42ee75ad25174c333981031f
Nelson Glardon <nelson.glardon@epfl.ch>
2023-04-17 14:18:38 +0200

    Update of model class
    The Embedding layer (applying to the channel layer) was added to the model. In order for the model to find the column corresponding to the channel, the constructor of the model now receives the list of the input variables

--- dnn_tau.py
+++ dnn_tau.py
@@ -1,12 +1,22 @@
 from tensorflow.python.keras import Model
-from tensorflow.python.keras.layers import Dense, Dropout
+from tensorflow.python.keras.layers import Dense, Dropout, Embedding
 from typing import List
-from tensorflow import function
+from tensorflow import function, concat
 
 class Dnn_tau(Model):
-	def __init__(self, depths=[10,10,10,10], activation='relu', output_size = 1):
+	def __init__(self, input_vars, depths=[10,10,10,10], n_channels=5, ch_emb_dim=3, activation='relu', output_size = 1):
+		"""
+		Arguments : 
+			-input_vars : list of keys of the dataset in the right order
+			-depths : len(depths) = number of hidden layers. The ith element of depths corresponds to the depth of the ith hidden layer
+			-n_channels : number of possible channel considered for the events in the datasets
+			-ch_emb_dim : dimension of the embedding layer for the various channels
+			-actiavtion : activation fonction between the hidden layers
+			-output_size : dimension of the output
+		"""
 		super().__init__()
 
+		self.input_vars = input_vars
 		
 		if type(depths) != list:
 			depths = [depths]
@@ -21,10 +31,16 @@ class Dnn_tau(Model):
 		else:
 			last_act = 'softmax'
 		self.output_ = Dense(units=output_size, activation=last_act)
+		self.emb = Embedding(n_channels, ch_emb_dim)
       
 	@function
 	def call(self, x):
-		print(type(x))
+		# Embedding of the channel column
+		ch_var_idx = self.input_vars.index('channel')
+		ch_emb = self.emb(x[:,ch_var_idx])
+		x = concat([x[:,:ch_var_idx], ch_emb, x[:,ch_var_idx+1:]], axis=1)
+
+		# Apply the layers to the data tensor
 		for layer in self.layers_:
 			x = layer(x)
 		return self.output_(x)
