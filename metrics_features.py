907e904c2a86d1bb51c1222254047a489049f24f
Nelson Glardon <glardon@lphepc119.epfl.ch>
2023-05-01 23:58:51 +0200

    Update of metrics_features.py and feature_importance_analysis.ipynb
    The function fi_perm was corrected, to correct the warning appearing during the assignement of the shuffled column. This function was successfully tested in feautre_importance_analysis.ipynb

--- metrics_features.py
+++ metrics_features.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import pandas as pd
-import numpy as np
+from numpy.random import permutation
 from copy import deepcopy
 from sklearn.feature_selection import mutual_info_regression
 
@@ -9,26 +9,18 @@ def fi_perm(model, test_df, input_vars, keys_shuffle):
     if type(keys_shuffle) != list:
         keys_shuffle = [keys_shuffle]
 
-    x_test = test_df[input_vars]
-    print("x_test defined")
-    y_test = x_test.pop('signal_label').astype(float)
-    print("y_test defined")
-    
-    loss_no_shuffle, _ = model.evaluate(x_test, y_test, sample_weight=test_df['genWeight'], verbose=0)
-    print("Loss without shuffle evaluated")
-    for key in keys_shuffle:
-        x_test[key] = np.random.permutation(x_test[key])
-    loss_shuffle, _ = model.evaluate(x_test, y_test, sample_weight=test_df['genWeight'], verbose=0)
-    print("Loss with shuffle evaluated")
+    test = deepcopy(test_df)
 
-    return loss_no_shuffle, loss_shuffle
-'''
-To Do : 
-    -adapt to have loss as a metric (search what 'evaluate' returns) and correct (what is called loss above is in fact the score I think)
-    -Understand why the evaluation takes so long 
-    -Check that the result in feature_importance_analysis.ipynb is correct
-    and then work on fi_mutual_info
-'''
+    x_test = test[input_vars]
+    y_test = x_test.pop('signal_label').astype(float)
+    
+    for key in keys_shuffle:
+        test[key] = permutation(test[key])
+        x_test = test[input_vars]
+        y_test = x_test.pop('signal_label').astype(float)
+    loss_shuffle, _ = model.evaluate(x_test, y_test, sample_weight=test_df['genWeight'], verbose=0)
+
+    return loss_shuffle
 
 
 def fi_mutual_info(model, test_df, input_vars):
