c7a6e2191797afc57516c6f6f2d9f550e9fec5e6
Nelson Glardon <glardon@lphepc119.epfl.ch>
2023-05-29 11:57:44 +0200

    Update of compute_feature_importance.py and modification for use.

--- compute_feature_importance.py
+++ compute_feature_importance.py
@@ -1,5 +1,5 @@
 from utils import split_dataset
-from TP4b.metrics import fi_perm
+from metrics import fi_perm
 import pandas as pd
 import pickle
 from data_extractor import output_vars_v4
@@ -17,24 +17,26 @@ def complete_path(rel_path):
     path = os.path.join(current_directory, rel_path)
     return path
 
-train = pd.read_pickle(complete_path('extracted_data/TEST9_global_v4_train'))
-val = pd.read_pickle(complete_path('extracted_data/TEST9_global_v4_val'))
-test = pd.read_pickle(complete_path('extracted_data/TEST9_global_v4_test'))
-meas = pd.read_pickle(complete_path('extracted_data/TEST9_global_v4_meas'))
+name = 'TEST9_global_v4_all_channels'
+save_path = 'saved_results/new_results/'
+
+train = pd.read_pickle(complete_path('extracted_data/TEST9_global_v4_or&norm_weights_train'))
+val = pd.read_pickle(complete_path('extracted_data/TEST9_global_v4_or&norm_weights_val'))
+test = pd.read_pickle(complete_path('extracted_data/TEST9_global_v4_or&norm_weights_test'))
+meas = pd.read_pickle(complete_path('extracted_data/TEST9_global_v4_or&norm_weights_meas'))
 
 file_path_idx = complete_path('extracted_data/TEST9_global_v4_all_normalized_channel_indices')
 with open(file_path_idx, 'rb') as file:
     channel_indices = pickle.load(file)
 
-with open(complete_path('saved_results/features&label_sel1'), 'rb') as file:
+with open(complete_path('saved_results/features&label_all_v4'), 'rb') as file:
     features = pickle.load(file)
 
 to_permute = deepcopy(features)
 to_permute.remove('signal_label')
 to_permute.remove('channel')
 
-# model = tf.keras.models.load_model(complete_path('saved_models/TEST9_global_v4_all_channels_depth_10'))
-model = tf.keras.models.load_model(complete_path('saved_models/TEST9_sel1_depth_2'))
+model = tf.keras.models.load_model(complete_path('saved_models/'+name+'_depth_3'))
 
 def compute_fi_perm(model, test, var_names, to_permute, name, channel):
 
@@ -48,14 +50,14 @@ def compute_fi_perm(model, test, var_names, to_permute, name, channel):
         delta_loss_perm.append([loss_shuffle-loss_no_shuffle])
     delta_loss_perm = dict(zip(features, delta_loss_perm))
 
-    filename = complete_path("saved_results/" + name + "_loss_shuffle_" + channel)
+    filename = complete_path(save_path + name + "_loss_shuffle_" + channel)
     with open(filename, "wb") as file:
         pickle.dump(delta_loss_perm, file)
 
 for channel in channel_indices:
     print(channel)
     test_channel = test[test['channel'] == channel_indices[channel]]
-    compute_fi_perm(model, test_channel, features, to_permute, 'TEST9_sel1_test', channel)
+    compute_fi_perm(model, test_channel, features, to_permute, name, channel)
 
 
 
